91bb110d00b22f0826e28bd00e2553c113d5514e
Mbabazi-Angel <144022384+Mbabazi-Angel@users.noreply.github.com>
2023-09-12 20:39:14 +0300

    programming in python class assignment

---
+++ python programming work.py
@@ -0,0 +1,32 @@
+# Initialise a variable to store the sum
+# sum of even numbers less than 8
+evennumbers = 2,4,6
+sumofevennumbers = (2 + 4 + 6)
+print(type(sumofevennumbers))
+print("The sum of even numbers less than 8 is" + " " +str(sumofevennumbers))
+
+
+# Initialise a variable to store the average
+# average of 5 numbers from the keyboard 
+numbers = (3,5,7,9,6)
+totalofnumbers = (3 + 5 + 7 + 9 + 6)
+average = (totalofnumbers)/5
+print(type(average))
+print("The average of 5 numbers from the keyboard is" + " " + str(average))  
+
+
+output = -5 + 8 * 6
+print(type(output))
+print("-5 + 8 * 6 =" + " " + str(output))
+
+output = (55 + 9) % 9
+print(type(output))
+print("(55 + 9) % 9 =" + " " + str(output) )
+
+output = 20 + -3 * 5 / 8
+print(type(output))
+print("20 + -3 * 5 / 8 =" + " " + str(output))
+
+output = 5 + 15 / 3 * 2 - 8 % 3
+print(type(output))
+print("5 + 15 / 3 * 2 - 8 % 3 =" + " " + str(output))
